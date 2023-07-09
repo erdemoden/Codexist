1833e38df05408b71ff46e55cfcf17f81a055b79
erdemoden <erdemoden5@gmail.com>
2023-07-09 20:11:49 +0300

    react.js ufak tasarım

--- React/codexist-front/src/App.jsx
+++ React/codexist-front/src/App.jsx
@@ -2,33 +2,30 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import {GoogleMap, InfoWindow, Marker, MarkerF, useLoadScript} from "@react-google-maps/api";
 function App() {
-  const [count, setCount] = useState(0)
-
+  //const [count, setCount] = useState(0)
+  const {isLoaded} = useLoadScript({
+    googleMapsApiKey:"AIzaSyBfHOXW8c7QiN6_iJR6Hx5V7r8RYpnqBps",
+  });
+  if(!isLoaded)return (<div>Loading....</div>)
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <p> Deneme</p>
+    <h2 className='title'>Search Nearby Restaurants</h2>
+    <div className='search'>
+    <label for="latitude" className='labels'>Latitude:</label>
+    <input type='text'id='latitude'name='latitude' className='texts'></input>
+    <label for="longitude" className='labels'>Longitude:</label>
+    <input type='text'id='longitude'name='longitude' className='texts'></input>
+    <label for="radius" className='labels'>Radius:</label>
+    <input type='text'id='radius'name='radius' className='texts'></input>
+    </div>
+    <button className='btn btn-success btnpos'>deneme</button>
+    
+        <GoogleMap zoom={10} center={{lat:44,lng:-80}} mapContainerClassName="map">
+          <MarkerF InfoWindow={{content:"merhaba"}} position={{lat:44,lng:-80}}label={{color:"black",text:"HacıOğlu Lahmacun Kebap",fontSize:"15px",className:"deneme"}}></MarkerF>
+        </GoogleMap>
+       
     </>
   )
 }
